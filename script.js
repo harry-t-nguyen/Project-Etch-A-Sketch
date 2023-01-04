f25b6bd0022832fe318c89854fc2dce4309d9cec
harry-t-nguyen <harrytnguyeen@gmail.com>
2023-01-05 02:40:52 +1100

    Square changes dynamically based on input

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let numberofsquare = document.querySelector('#input').value;
-
 function makegrid(x){
     let grid = document.querySelector('#grid');
     grid.style.cssText = `display: grid; grid-template-columns: repeat(${x},auto)`
 
+    while (grid.firstChild){
+        grid.firstChild.remove();
+    };
+    
     for (let i=0; i<x; i++){
         for (let j = 0; j<x; j++){
             let griditems = document.createElement('div');
@@ -13,25 +15,32 @@ function makegrid(x){
     }
 };
 
-makegrid(numberofsquare);
-// const black = document.querySelector('#black');
-// black.addEventListener('click', () =>{
-//     return color = 'black'
-// });
+const inputvalue = document.querySelector('#input');
+inputvalue.addEventListener("change", () =>{
+    let numberofsquare = inputvalue.value;
+    console.log(numberofsquare);
+    makegrid(numberofsquare);
 
-
-const griditems = document.querySelectorAll('.griditems');
-griditems.forEach((griditem) => {
-    griditem.addEventListener('mouseover', () => {
-        griditem.style.cssText = 'background-color: black;'
-})});
-
-
-// square to white colour when click clear
-const clear = document.querySelector('#clear');
-clear.addEventListener('click',() => {
+    // hover to black
+    const griditems = document.querySelectorAll('.griditems');
     griditems.forEach((griditem) => {
-        griditem.style.cssText = `background-color: white;`
-    })
+    griditem.addEventListener('mouseover', () => {
+    griditem.style.cssText = 'background-color: black;'
+    })});
+
+    // square to white colour when click clear
+    const clear = document.querySelector('#clear');
+    clear.addEventListener('click',() => {
+    griditems.forEach((griditem) => {
+        griditem.style.cssText = `background-color: white;`})
+    });
 });
+
+
+
+
+
+
+
+
 
